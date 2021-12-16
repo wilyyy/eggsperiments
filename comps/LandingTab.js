7f481deb1e28b033a3027b2d804806d8f940800e
wilyyy <alvarez_william@outlook.com>
2021-12-16 03:32:42 -0800

    neumorphism bro xd

--- comps/LandingTab.js
+++ comps/LandingTab.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
-    
+const Container = styled.button`
+    min-width: 300px;
+    min-height: 66px;
+    border-radius: 20px;
+    background-color: #24282E;
+    box-shadow: -2.17893px -2.17893px 10.5368px #2F343C, 2.17893px 2.17893px 10.5368px #171A1E;
 `;
 
 const LandingTab = () => {
     return (
-        <div></div>
+        <Container>
+            this looks so bad lol
+        </Container>
     )
 }
 
