7b6010fadb7a6a003c8bc1c9af8a19c61bd21057
wilyyy <alvarez_william@outlook.com>
2021-12-23 16:11:15 -0800

    last lil bit b4 break time

--- comps/AboutSection.js
+++ comps/AboutSection.js
@@ -1,15 +1,30 @@
 import styled from 'styled-components';
 import { COLORS } from '../styles/Colors';
 
+import LottieAnim from './LottieAnim';
+import AboutCard from './AboutCard';
+
+const Container = styled.div`
+    width: 100%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    font-family: "MBFNanomaton";
+`;
+
 const H1 = styled.h1`
 
 `;
 
 const AboutSection = () => {
     return (
-        <div>
-            asdsadad
-        </div>
+        <Container>
+            <H1>About</H1>
+            <LottieAnim />
+            <AboutCard />
+        </Container>
     )
 }
 
