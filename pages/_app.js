4d761aa39f3df7a8b831f5f6b8a3b48d7a8a46cc
wilyyy <alvarez_william@outlook.com>
2021-12-30 00:45:05 -0800

    dark mode works holy guacamoley im a god

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,30 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import Link from "next/link";
+import { useState } from "react";
 import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "../styles/ThemeConfig";
+import styled from 'styled-components';
+
+import LightSwitch from '../comps/LightSwitch';
+
+const LightCont = styled.div`
+  position: absolute;
+  top: 30px;
+  left: 65%;
+`;
 
 //add metadata later
 
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    theme == 'dark' ? setTheme('light') : setTheme('dark')
+  }
+
   return (
-    <div>
-      <Component {...pageProps} />
+    <ThemeProvider theme={theme == 'dark' ? darkTheme : lightTheme}>
       <Head>
         <title>Eggsperiments</title>
         <link
@@ -24,7 +40,13 @@ function MyApp({ Component, pageProps }) {
             crossOrigin=""
         />
       </Head>
-    </div>
+      <Component {...pageProps} />
+
+      <LightCont>
+        <LightSwitch onButtonPress={toggleTheme} />
+      </LightCont>
+
+    </ThemeProvider>
     
   )
 }
