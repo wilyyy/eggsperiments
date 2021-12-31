4498aee4f76b9df57518717077b23c414eb20faa
wilyyy <alvarez_william@outlook.com>
2021-12-30 17:53:39 -0800

    store finished, ready for mapping

--- pages/Store.js
+++ pages/Store.js
@@ -2,8 +2,35 @@ import styled from 'styled-components';
 import { useState } from "react";
 import { COLORS } from '../styles/Colors';
 
+import NavDesktop from '../comps/NavDesktop';
+import StoreEggCard from '../comps/StoreEggCard';
+
+const Page = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+`;
+
+const PageCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: ${({ theme }) => theme.BASE};
+    padding: 0 0 0 2%;
+`;
+
 const Store = () => {
-    return <></>
+    return (
+        <Page>
+            <NavDesktop />
+            <PageCard>
+                <StoreEggCard />
+            </PageCard>
+        </Page>
+    )
 }
 
 export default Store;
