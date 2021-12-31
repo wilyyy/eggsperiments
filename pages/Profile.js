5c3c4226ee9349bce0b4261f5f3194d1774959a7
wilyyy <alvarez_william@outlook.com>
2021-12-30 21:14:29 -0800

    all routing done & pages init

--- pages/Profile.js
+++ pages/Profile.js
@@ -2,8 +2,30 @@ import styled from 'styled-components';
 import { useState } from "react";
 import { COLORS } from '../styles/Colors';
 
+import NavDesktop from '../comps/NavDesktop';
+
+const Page = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+`;
+
+const PageCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    background: ${({ theme }) => theme.BASE};
+    padding: 0 0 0 2%;
+`;
+
 const Profile = () => {
-    return <></>
+    return (
+        <Page>
+            <NavDesktop />
+            <PageCard>Profile not mvp</PageCard>
+        </Page>
+    )
 }
 
 export default Profile;
